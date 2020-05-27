8178fef25c875d59a2f3a3d87497a58728c75abc
Adnan <adnanhussainturki@gmail.com>
2020-05-28 01:02:01 +0530

    Fix

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ rm -rf $BACKUPDIR
 if [ -d $DIR ] 
 then
     echo "Making backup of previous $NAME." 
-    mv -f $DIR $BACKUPDIR
+    mv -rf $DIR $BACKUPDIR
 else
     echo "No previous $NAME is found. Skipping backup."
 fi
