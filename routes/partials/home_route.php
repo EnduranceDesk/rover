5b5421f1eea3fd6be699b62d927ab4b82ae4b0b3
Adnan <adnanhussainturki@gmail.com>
2022-02-15 21:14:20 +0530

    Test commit

--- routes/partials/home_route.php
+++ routes/partials/home_route.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', 'HomeController@index')->name('home.direct')->middleware("auth"); 
-Route::get('/home', 'HomeController@index')->name('home')->middleware("auth"); 
+Route::get('/', 'HomeController@index')->name('home.direct')->middleware("auth");
+Route::get('/home', 'HomeController@index')->name('home')->middleware("auth");
 
